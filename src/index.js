02f8227235117c70c56c486013d8fc47bb3bf5b8
rushikeshpatil1007 <patilrushikesh1007@gmail.com>
2023-05-07 23:39:24 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -7,15 +7,30 @@ const port = process.env.port || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Connect to DATABASE
 const DATABASE_URL = "mongodb://127.0.0.1:27017";
-mongoose.connect(DATABASE_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-const db = mongoose.connection;
-db.on("error", (err) => console.log(err));
-db.once("open", () => console.log("connected to database"));
+mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true, })
+  .then((result) => {
+    console.log("connected to database");
+    app.listen(port, () => console.log(`App listening on port ${port}!`));
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-// Start Server
-app.listen(port, () => console.log(`App listening on port ${port}!`));
+
+
+// // Connect to DATABASE
+// const DATABASE_URL = "mongodb://127.0.0.1:27017";
+// mongoose.connect(DATABASE_URL, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+// const db = mongoose.connection;
+// db.on("error", (err) => console.log(err));
+// db.once("open", () => console.log("connected to database"));
+
+// // Start Server
+
+// app.listen(port, () => console.log(`App listening on port ${port}!`));
+
+
